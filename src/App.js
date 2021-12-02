2cd611df3a597c21709000af6aebda1f13f0680a
tammytom <taalam00795@gmail.com>
2021-12-03 03:23:07 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+// import logo from "./logo.svg"
+import Data from "./Data.json"
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      {/* <img className="bg-img" src="./images/trees-6207925.jpg" alt=""></img>; */}
+      {Data.map((data, key) => {
+        return (
+          <div className="section" key={key}>
+            {/* <div className="topone">
+              Welcome to my world.
+            </div> */}
+            <div style={{ width: "100%" }}>
+              <p className="paragraph"> {data.para}</p>
+              <h1 className="heading">{data.title}</h1>
+            </div>
+            <div className="pic" style={{ width: "50%" }}>
+              {/* <img src={data.ImagePath} alt="img" /> */}
+            </div>
+          </div>)
+      })}
     </div>
   );
 }
